23279014704ccaf43d641cc48cb55589acac22d4
Aygül <gulnihalaygul@gmail.com>
2026-04-05 18:55:23 +0300

    Update her şey

--- models/Channel.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-
-const channelSemasi = new mongoose.Schema({
-  name: { type: String, required: true },
-  description: { type: String, default: '' }
-}, { timestamps: true })
-
-module.exports = mongoose.model('Channel', channelSemasi)
